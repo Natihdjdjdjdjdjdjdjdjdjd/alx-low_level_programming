f266229d2fb390c7b3ecb4c4ad3a67f530c7650f
Natoli <natasbest@gmail.com>
2023-06-15 14:38:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
 #include  <stdio.h>
 
 /**
- * main - entry point of the program
- *
- * dicripiton: this is  a c program that prints a line with in put function
- *
- * return: always 0  (success)
+ * main - A program that prints a line with puts function
+ * Return: always 0  (success)
 */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
