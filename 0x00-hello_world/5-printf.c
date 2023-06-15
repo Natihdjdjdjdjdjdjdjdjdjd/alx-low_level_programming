2b393c5fe3f2a29f4c5fc02e1c76595610e5b218
Natoli <natasbest@gmail.com>
2023-06-15 14:19:40 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stduio>
+/**
+ * main - A program that print aline using the printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
