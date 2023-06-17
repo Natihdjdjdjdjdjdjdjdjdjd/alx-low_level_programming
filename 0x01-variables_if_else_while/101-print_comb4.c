c50d040deaa91f0840b0717dc9ca5f3b45906021
Natoli <natasbest@gmail.com>
2023-06-17 15:11:24 -0700

    ./101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,9 +12,9 @@ int main(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y < 10; y++)
+		for (y = x + 1; y < 10; y++)
 		{
-			for (z = 0; z < 10; z++)
+			for (z = y + 1; z < 10; z++)
 			{
 				if ((y != x) != z)
 				{
