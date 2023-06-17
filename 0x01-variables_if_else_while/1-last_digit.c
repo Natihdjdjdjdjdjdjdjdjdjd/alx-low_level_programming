003626131354c1bd3535614baacf6daefecdcbf9
Natoli <natasbest@gmail.com>
2023-06-16 22:33:03 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - generate my entry point
+ * Return: always return 0 (success)
+*/
+int main(void)
+{
+	int n;
+	int x;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	x = n % 10;
+	if (x > 0)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, x);
+	}
+	else if (x == 0)
+	{
+		printf("Last digit of %d is %d is 0\n", n, x);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is lesthan 6 and not 0\n", n, x);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entery point a program that prints the alphabet in lowercase, followed by a new line
+ *Return: always 0 (ssuccess)
+ */
+int main(void)
+	int n =97;
+{
+	while (n <= 122)
+	
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}	
