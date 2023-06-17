fdb013ac260e49c46f7e7e894da6eae88b81f131
Natoli <natasbest@gmail.com>
2023-06-17 02:13:57 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - A program that print the alphabet in lowercase except q and e
+ * Return: always 0 (succeses)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if (n != 'a' && n != 'q')
+			putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
