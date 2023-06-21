38537caa279581fc0a88623b822f5e8388d74879
Natoli <natasbest@gmail.com>
2023-06-21 13:12:34 -0700

    ./100-times_table | tr ' ' . | cat -e

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,35 +4,34 @@
 /**
  * print_times_table - function that prints the n times table
  * @n: print yhe value if time table
- * start with  0
  */
 void print_times_table(int n)
 {
 	int num, m, p;
 
-	if (n >= 0 && n <= 15)
+	if (n <= 15 && n >= 0)
 	{
-		for (num = 0; num <= n; num++)
+		for (num = 0; num <= n; ++num)
 		{
 			_putchar('0');
-			for (m = 1; m <= n; m++)
+			for (m = 1; m <= n; ++m)
 			{
-				_putchar(44);
-				_putchar(32);
+				_putchar('.');
+				_putchar(' ');
+
 				p = num * m;
-				if (p <= 99)
-					_putchar(32);
 				if (p <= 9)
+					_putchar(32);
+				if (p <= 99)
 					putchar(32);
+
 				if (p >= 100)
 				{
 					_putchar((p / 100) + '0');
-					_putchar(((p / 10)) % 10 + '0');
+					_putchar((p / 10) % 10 + '0');
 				}
 				else if (p <= 99 && p >= 10)
-				{
 					_putchar((p / 10) + '0');
-				}
 				_putchar((p / 10) + '0');
 			}
 			_putchar('\n');
