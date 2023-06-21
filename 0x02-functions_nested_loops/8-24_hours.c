2504b71eacfc8049456994ba5a394e94776e0b68
Natoli <natasbest@gmail.com>
2023-06-21 01:52:14 -0700

    ./8-24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *void jack_bauer - function that prints every minute of the day of Jack Baue
+ * Return: always 0
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a < 3; a++)
+	{
+		for (b = 0; b < 10; b++)
+	{
+		if ((a < 2 && b < 10) || (a < 3 && b < 4))
+		{
+			for (c = 0; c <= 5; c++)
+			{
+				for (d = 0; d < 10; d++)
+			{
+				_putchar(a + '0');
+				_putchar(b + '0');
+				_putchar(58);
+				_putchar(c + '0');
+				_putchar(d + '0');
+				_putchar('\n');
+			}
+			}
+		}
+	}
+	}
+}
