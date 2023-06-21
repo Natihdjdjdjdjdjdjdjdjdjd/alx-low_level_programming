643d7ab616d0e6fc8b29c0017d4fc1930c7d6357
Natoli <natasbest@gmail.com>
2023-06-21 15:05:13 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,35 +9,32 @@ void print_times_table(int n)
 {
 	int num, m, p;
 
-	if (n <= 15 && n >= 0)
+	if (n >= 0 && n <= 15)
 	{
-		for (num = 0; num <= n; ++num)
+		for (num = 0; num <= n; num++)
 		{
-			_putchar(48);
-			for (m = 1; m <= n; ++m)
+			_putchar('0');
+			for (m = 1; m <= n; m++)
 			{
 				_putchar(',');
 				_putchar(' ');
-
 				p = num * m;
-				if (p <= 9)
+				if (p <= 90)
 					_putchar(' ');
-				if (p <= 99)
-					putchar(' ');
-
+				if (p <= 9)
+					putchar('  ');
 				if (p >= 100)
 				{
-					_putchar((p / 100) + 48);
-					_putchar((p / 10) % 10 + 48);
+					_putchar((p / 100) + '0');
+					_putchar((p / 10) % 10 + '0');
 				}
-				else if (p <= 99 && p >= 10)
+				else if (p <= 90 && p >= 10)
 				{
-					_putchar((p / 10) + 48);
-				}
-				_putchar((p % 10) + 48);
+					_putchar((p / 10) + '0');
+	}
+				_putchar((p % 10) + '0');
 			}
 			_putchar('\n');
 		}
 	}
 }
-
