c4109d75b6d151b91a40b4142ef8b248f31c59b9
Natoli <natasbest@gmail.com>
2023-06-30 11:08:50 -0700

    ./6-cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,7 +3,7 @@
 /**
  * *cap_string - function that capitalizes all words of a string.
  * @n: the capitalized string
- * Return: to n
+ * Return: to string n.
  */
 char *cap_string(char *n)
 {
@@ -13,21 +13,23 @@ char *cap_string(char *n)
 	{
 		while (!(n[x] >= 'a' && n[x] <= 'z'))
 		x++;
+
 		if (n[x - 1] == ' ' ||
 				n[x - 1] == '\t' ||
-				 n[x - 1] == '\n' ||
-				  n[x - 1] == ',' ||
-				   n[x - 1] == ';' ||
-				    n[x - 1] == '.' ||
-				     n[x - 1] == '!' ||
-				      n[x - 1] == '?' ||
-				       n[x - 1] == '"' ||
-					n[x - 1] == '(' ||
-					 n[x - 1] == ')' ||
-					  n[x - 1] == '{' ||
-					   n[x - 1] == '}' ||
-					    x == 0)
-					   n[x] -= 32;
+				n[x - 1] == '\n' ||
+				n[x - 1] == ',' ||
+				n[x - 1] == ';' ||
+				n[x - 1] == '.' ||
+				n[x - 1] == '!' ||
+				n[x - 1] == '?' ||
+				n[x - 1] == '"' ||
+				n[x - 1] == '(' ||
+				n[x - 1] == ')' ||
+				n[x - 1] == '{' ||
+				n[x - 1] == '}' ||
+				x == 0)
+			n[x] -= 32;
+		x++;
 	}
 	return (n);
 }
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *leet - function that encodes a string into 1337.
+ * @x: entr sirtng
+ * Return: at void
+ */
+char *leet(char *)
+{
+
